ed2ae62d9afd8f5c9b8b07f5bf8497d4fd7c512b
uikoo9 <uikoo9@qq.com>
2021-11-24 16:47:15 +0800

    split chunks plugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: {
-      import: './src/index.js',
-      dependOn: 'shared',
-    },
-    another: {
-      import: './src/another-module.js',
-      dependOn: 'shared',
-    },
-    shared: 'lodash',
+    index: './src/index.js',
+    another: './src/another-module.js',
   },
   devtool: 'inline-source-map',
   plugins: [
@@ -26,6 +19,8 @@ module.exports = {
     clean: true,
   },
   optimization: {
-    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
