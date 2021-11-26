f82312bec01abd44d3ce2241df478bd506c2d821
uikoo9 <uikoo9@qq.com>
2021-11-26 19:29:59 +0800

    lib init

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Caching',
-    }),
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-     cacheGroups: {
-       vendor: {
-         test: /[\\/]node_modules[\\/]/,
-         name: 'vendors',
-         chunks: 'all',
-       },
-     },
-   },
+    filename: 'webpack-numbers.js',
+    library: {
+      name: 'webpackNumbers',
+      type: 'umd',
+    },
   },
 };
