714a6321645ffc5f4952e0e55004f80ae384b23c
uikoo9 <uikoo9@qq.com>
2021-12-07 17:28:51 +0800

    lazy load

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import { cube } from './math.js';
+import _ from 'lodash';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
 function component() {
-  const element = document.createElement('pre');
+  const element = document.createElement('div');
+  const button = document.createElement('button');
+  const br = document.createElement('br');
 
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
+  button.innerHTML = 'Click me and look at the console!';
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    const print = module.default;
+
+    print();
+  });
 
   return element;
 }
--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Production',
+      title: 'Lazy Loading',
     }),
   ],
   output: {
