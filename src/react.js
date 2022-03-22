a123198b3b080370c6f90147957cd04b411c3c6d
uikoo9 <uikoo9@qq.com>
2022-03-22 18:00:33 +0800

    feat: add sass

--- src/react.js
+++ src/react.js
@@ -1,5 +1,11 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+'use strict';
+
+// react
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// scss
+import './react.scss';
 
 ReactDOM.render(
   <h1>Helloo, world!</h1>,
--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,14 @@ module.exports = {
   module: {
     rules: [
       reactRule,
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
     ],
   },
   output: {
